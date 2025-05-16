8b1048fb9b56d223d74acc96f30eb724b34ce20f
Eliandro <eliandro.gomes42@gmail.com>
2025-05-16 16:28:59 -0300

    MergeSort adicionado

--- src/br/com/gomes/main/Main.java
+++ src/br/com/gomes/main/Main.java
@@ -1,6 +1,7 @@
 package br.com.gomes.main;
 
 import br.com.gomes.service.Ordenador;
+import br.com.gomes.service.strategies.MergeSort;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,14 +11,18 @@ public class Main {
         int[] arrayNum = {5, 3, 8, 1, 2};
 
         System.out.printf("[insertionsort] ou [insertion] - Insertion\n" +
-                "[selectionsort] ou [selection] - Insertion\n" +
-                "[qualquer outra entrada] - BubbleSort\n" +
+                "[selectionsort] ou [selection] - Selection\n" +
+                "[mergesort] ou [merge] - Merge\n" +
+                "[qualquer outra entrada] - Bubble\n" +
                 "---[");
 
         String entrada = new Scanner(System.in).nextLine();
-        Ordenador ordenador = new Ordenador(entrada);
-        ordenador.ordenar(arrayNum);
-
-        System.out.println("Vetor ordenado: " + Arrays.toString(arrayNum));
+        if(entrada.equals("merge") || entrada.equals("mergesort")) {
+            MergeSort.ordenar(arrayNum);
+        } else {
+            Ordenador ordenador = new Ordenador(entrada);
+            ordenador.ordenar(arrayNum);
+            System.out.println("Vetor ordenado: " + Arrays.toString(arrayNum));
+        }
     }
 }
--- src/br/com/gomes/service/strategies/MergeSort.java
+++ src/br/com/gomes/service/strategies/MergeSort.java
@@ -1,20 +1,59 @@
 package br.com.gomes.service.strategies;
 
+import java.util.Arrays;
+
 public class MergeSort {
-    public static void main(String[] args) {
+    public static int[] ordenar(int[] array) {
+        int arrayLength = array.length;
 
-    }
+        if(arrayLength <= 1) return array;
 
-    private static void merge(int vetor[], int aux[], int inicio, int fim) {
-        int meio = (inicio + fim) /2;
-        if(inicio<fim) {
-            merge(vetor, aux, inicio, meio);
-            merge(vetor, aux, meio+1, fim);
-            intercalacao(vetor, aux, inicio, meio, fim);
+        int midIndex = arrayLength / 2;
+        int[] leftArray = new int[midIndex];
+        int[] rightArray = new int[arrayLength-midIndex];
+
+        for(int i = 0; i < midIndex; i++) {
+            leftArray[i] = array[i];
         }
+        for(int i = midIndex; i < arrayLength; i++) {
+            rightArray[i - midIndex] = array[i];
+        }
+
+        ordenar(leftArray);
+        ordenar(rightArray);
+
+        merge(array, leftArray, rightArray);
+
+        System.out.println("Merge usado: "+ Arrays.toString(array));
+        return array;
     }
 
-    public static void intercalacao(int vetor[], int aux[], int inicio, int meio, int fim) {
+    private static void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
 
+        int l = 0, r = 0, a = 0;
+        while(l < leftSize && r < rightSize) {
+            if(leftArray[l] < rightArray[r]) {
+                array[a] = leftArray[l];
+                l++;
+            } else {
+                array[a] = rightArray[r];
+                r++;
+            }
+            a++;
+        }
+
+        while(l < leftSize) {
+            array[a] = leftArray[l];
+            l++;
+            a++;
+        }
+
+        while(r < rightSize) {
+            array[a] = rightArray[r];
+            r++;
+            a++;
+        }
     }
 }
